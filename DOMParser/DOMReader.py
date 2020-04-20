95b61926164fdd2627002f83bf1b801bf526dcab
Sawyer Timperley <sawyertimperley@gmail.com>
2020-04-20 17:02:40 -0500

    XML transform comments and accessors

--- DOMParser/DOMReader.py
+++ DOMParser/DOMReader.py
@@ -3,9 +3,16 @@ from zipfile import ZipFile
 import os
 import shutil
 
+"""
+@author Sawyer Timperley
+@date   2020-04-20
+@title  KML Transforming Module With File IO
+"""
 
+# TODO (someday) upgrade to XSLT (it's faster, has support for namespaces, and formats correctly)
 class DOMReader:
-    def __init__(self, inputKMZtemplate, inputKMZcompare, outputKMZdir, tempDir="./tmp"):
+    def __init__(self, inputKMZtemplate, inputKMZcompare, outputKMZdir="./processed/out.kmz", tempDir="./tmp"):
+        # user variables
         self.inputTemplate = inputKMZtemplate
         self.inputCompare = inputKMZcompare
         self.outputPath = outputKMZdir
@@ -15,22 +22,27 @@ class DOMReader:
         self.COMPARE_WORKING_PATH = self.workingDir + '/comparetmp'
         self.OUTPUT_WORKING_PATH = self.workingDir + '/outtmp'
 
+        # counter variables
         self.xmlData = ""
-
         self.conflictCount = 0
 
+    # unzip the kmz file to the temp directory, retaining the zip folder structure
     def extractKMZ(self, file, dest):
         with ZipFile(file, 'r') as zf:
             zf.extractall(dest)
 
+    # setup the temporary working directories
+    # TODO make this work without unnecessary disk usage
     def setupWorkingDir(self):
         self.extractKMZ(self.inputTemplate, self.TEMPLATE_WORKING_PATH)
         self.extractKMZ(self.inputCompare, self.COMPARE_WORKING_PATH)
 
+    # does not run automatically, as one can reconfigure the object using the accessors
     def process(self):
         self.conflictCount = 0
         self.setupWorkingDir()
-        # parse "original" xml document
+
+        # parse template xml document
         originalDataStruct = dom.parse('{}/doc.kml'.format(self.TEMPLATE_WORKING_PATH))
         docRootOriginal = originalDataStruct.documentElement
         templatePlaces = docRootOriginal.getElementsByTagName("Placemark")
@@ -40,56 +52,55 @@ class DOMReader:
         docRootNew = newDataStruct.documentElement
         comparePlaces = docRootNew.getElementsByTagName("Placemark")
 
+        # O(n^2), ouch. However, there is no easy way to sort the data based on this scope.
         for new_place in comparePlaces:
             append_flag = True  # if not set to false, append the new_place to the import folder
             test_name = new_place.getElementsByTagName("name")[0].firstChild.data
 
             for old_place in templatePlaces:
                 original_name = old_place.getElementsByTagName("name")[0].firstChild.data
-
-                # found the same entry name in the old one, so we can skip it from appending
+                # found the same entry name in the original, so we can skip it from appending
                 if str(test_name).lower() == str(original_name).lower():
                     append_flag = False
                     break
 
             if append_flag:
-                self.conflictCount += 1
-                folder = docRootOriginal.getElementsByTagName("Folder")
+                self.conflictCount += 1  # Used in later documentation
+                folder = docRootOriginal.getElementsByTagName("Folder")  # Look in all custom places folder struct
                 folderExists = False
-                folder_pointer = ""
+                folder_pointer = ""  # The import folder pointer
                 for name in folder:
                     if name.getElementsByTagName("name")[0].firstChild.data == "Import":
                         folderExists = True
                         folder_pointer = name
 
+                # create new xml folder entry if one does not exist
                 if not folderExists:
                     import_folder = originalDataStruct.createElement("Folder")
 
+                    # code block that builds the xml tree
                     folder_name = originalDataStruct.createElement("name")
                     folder_name.appendChild(originalDataStruct.createTextNode("Import"))
-
                     folder_open = originalDataStruct.createElement("open")
                     folder_open.appendChild(originalDataStruct.createTextNode("1"))
-
                     folder_desc = originalDataStruct.createElement("description")
                     folder_desc.appendChild(originalDataStruct.createTextNode("Imported from synchronization tool"))
-
                     import_folder.appendChild(folder_name)
                     import_folder.appendChild(folder_open)
                     import_folder.appendChild(folder_desc)
 
                     doc_root = originalDataStruct.getElementsByTagName("Document")[0]
-
                     doc_root.appendChild(import_folder)
-
                     folder_pointer = import_folder
 
-                # now append that sucker to the import folder!
+                # like setting up a window panel, place the tree structure in the root
                 folder_pointer.appendChild(new_place)
 
+        # save the raw xml data to memory, waiting to be written to.
         self.xmlData = originalDataStruct.toxml()
 
-    def writeOut(self):
+    # write the preprocessed xml to disk
+    def writeOut(self, clean=True):
         os.makedirs(self.OUTPUT_WORKING_PATH, exist_ok=True)
         output_file = open("{}/doc.kml".format(self.OUTPUT_WORKING_PATH), "w")
         output_file.write(self.xmlData)
@@ -102,9 +113,31 @@ class DOMReader:
             output_zip.write("{}/files".format(self.TEMPLATE_WORKING_PATH))
         output_zip.close()
 
+        if clean:
+            self.cleanWorkingDirectories()
+
     def cleanWorkingDirectories(self):
         shutil.rmtree(self.workingDir)
 
     # get the amount of additions after the last process() call
     def getAppendCount(self):
         return self.conflictCount
+
+    # class accessors (may remove if not used later)
+    def getTemplateKMZPath(self):
+        return self.inputTemplate
+
+    def getCompareKMZPath(self):
+        return self.inputCompare
+
+    def getOuputKMZPath(self):
+        return self.outputPath
+
+    def setTemplateKMZ(self, path):
+        self.inputTemplate = path
+
+    def setCompareKMZPath(self, path):
+        self.inputCompare = path
+
+    def setOutputKMZ(self, path):
+        self.outputPath = path

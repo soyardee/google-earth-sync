0a19c60c2abd52a96e8897b8733cdcc96c848209
Sawyer Timperley <sawyertimperley@gmail.com>
2020-04-21 16:16:33 -0500

    name insert test

--- tests/DBTest.py
+++ tests/DBTest.py
@@ -2,3 +2,6 @@ from kmlmodules.dbconnect import DBConnect
 
 db_interpreter = DBConnect("D:\Libraries\Google Drive\projects\code\python\kml-sync-tool\history")
 db_interpreter.createTables()
+db_interpreter.insert_test("bob")
+db_interpreter.insert_test("joe")
+db_interpreter.insert_test("bob")

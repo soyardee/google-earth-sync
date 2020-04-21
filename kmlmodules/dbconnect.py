470652ccd75a122cf7b782abbdbf32ac6824cd68
Sawyer Timperley <sawyertimperley@gmail.com>
2020-04-21 15:52:53 -0500

    init tables

---
+++ kmlmodules/dbconnect.py
@@ -0,0 +1,44 @@
+import sqlite3
+import os
+
+# controller class for the database (I/O interface)
+class DBConnect:
+    def __init__(self, dbpath="history", dbfile="data.db"):
+        os.makedirs(dbpath, exist_ok=True)
+        self.dbpath = os.path.join(dbpath, dbfile)
+        self.conn = sqlite3.connect(self.dbpath)
+
+    # create tables if none exist
+    def createTables(self):
+        c = self.conn.cursor()
+        create_names = "CREATE TABLE IF NOT EXISTS names  (" \
+            "user_id int(3) PRIMARY KEY NOT NULL, " \
+            "user_name varchar(30)); "
+        create_merges = "CREATE TABLE IF NOT EXISTS merges (" \
+            "merge_id int(16) PRIMARY KEY NOT NULL, " \
+            "user_id int(3) NOT NULL, " \
+            "time_stamp DATE, " \
+            "FOREIGN KEY(user_id) REFERENCES names(user_id)); "
+        create_files = "CREATE TABLE IF NOT EXISTS files  (" \
+            "merge_id int(16) NOT NULL," \
+            "templatefilename TEXT, " \
+            "appendfilename TEXT, " \
+            "outfilename TEXT, " \
+            "FOREIGN KEY(merge_id) REFERENCES merges(merge_id));"
+
+        c.execute(create_names)
+        c.execute(create_merges)
+        c.execute(create_files)
+
+    # insert a dictionary into the table
+    """
+    format: {
+                user_name_lower: string
+                timestamp: Date()
+                file1: string
+                file2: string
+                outfile: string
+                conflicts: int
+            }
+"""
+

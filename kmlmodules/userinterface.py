33cb2ac6649fc770e4842339d5893d61dcdc7689
Sawyer Timperley <sawyertimperley@gmail.com>
2020-05-02 22:26:27 -0500

    GUI framework complete

--- kmlmodules/userinterface.py
+++ kmlmodules/userinterface.py
@@ -6,15 +6,23 @@ from kmlmodules.domtransformer import DOMTransformer as xml
 import os
 
 
+class UserMain(ttk.Frame):
+    def __init__(self, parent):
+        # set up database object, stores all file processing history
+        self.db = DBConnect("data.db", "./history")
+
+        ttk.Frame.__init__(self, parent)
+
+        UserFrame(self).grid(column=0, row=0)
+        HistoryPanel(self, self.db).grid(column=0, row=1)
+
+        self.pack()
+
 class UserFrame(ttk.Frame):
     def __init__(self, parent):
         self.DIR_TEXT_SIZE = 50
 
         ttk.Frame.__init__(self, parent, padding="10 10 10 10")
-
-
-
-
 
         self.current_user = ""
         self.templatePath = ""
@@ -22,14 +30,11 @@ class UserFrame(ttk.Frame):
         self.outputPath = ""
         self.conflictCount = 0
 
-        # set up database object, stores all file processing history
-        #self.db = DBConnect("data.db", "./history")
-
 
         ttk.Label(self, text="Original KMZ File:").grid(column=0, row=0, sticky=tk.W)
         self.templateFilePath = tk.StringVar()
         ttk.Entry(self, width=self.DIR_TEXT_SIZE, textvariable=self.templateFilePath).grid(column=0, row=1, sticky="ew")
-        ttk.Button(self, width=10, text="Browse...", command=self.askopenfiletemplate).grid(column=1, row=1, sticky=tk.E)
+        ttk.Button(self, width=10, text="Browse...", command=self.askopenfiletemplate).grid(column=1, row=1)
 
         ttk.Label(self, text="Comparison KMZ File:").grid(column=0, row=2, sticky=tk.W)
         self.copyFilePath = tk.StringVar()
@@ -42,22 +47,18 @@ class UserFrame(ttk.Frame):
         self.outFilePath = tk.StringVar()
         ttk.Entry(self, textvariable=self.outFilePath).grid(column=0, row=6, sticky="ew")
         ttk.Button(self, width=10, text="Browse...", command=self.askopenoutputfile).grid(column=1, row=6)
+        ttk.Label(self, text="User Name:").grid(column=0, row=8, sticky=tk.W)
+        self.userNameEntry = tk.StringVar()
+        ttk.Entry(self, textvariable=self.userNameEntry).grid(column=0, row=9, sticky=tk.W)
 
+        ttk.Button(self, text="Convert", width=10, command=self.processXML).grid(column=1, row=9)
 
+        ttk.Separator(self, orient="horizontal").grid(column=0, row=10, columnspan=3, sticky="ew", pady=10)
 
-
+        ttk.Label(self, text="History").grid(column=0, row=11, sticky="w")
 
         self.pack()
 
-
-    def writedb(self):
-            self.db.insert_history(
-                self.current_user,
-                self.conflictCount,
-                os.path.basename(self.templatePath),
-                os.path.basename(self.copyPath),
-                os.path.basename(self.outputPath)
-            )
 
     def askopenfiletemplate(self):
         path = filedialog.askopenfilename(initialdir= "../", title="Select template file", filetypes=(("kmz files", "*.kmz"),))
@@ -77,13 +78,56 @@ class UserFrame(ttk.Frame):
         self.outFilePath.set(self.outputPath)
         return
 
+    def processXML(self):
+        return
+
+
+
+class HistoryPanel(ttk.Frame):
+    def __init__(self, parent, connection):
+        self.parent = parent
+        self.conn = connection
+
+        ttk.Frame.__init__(self, parent)
+
+        self.text = tk.Listbox(self, width=60)
+
+        for i in range(100):
+            self.text.insert(tk.END, str(i))
+
+        scrollbar = ttk.Scrollbar(self)
+
+        scrollbar.config(command=self.text.yview)
+        self.text.config(yscrollcommand=scrollbar.set)
+
+        self.text.pack(side=tk.LEFT, fill=tk.BOTH)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+
+
+    def getHistoryStringList(self):
+        return
+
+
+
+    def writedb(self):
+            self.db.insert_history(
+                self.current_user,
+                self.conflictCount,
+                os.path.basename(self.templatePath),
+                os.path.basename(self.copyPath),
+                os.path.basename(self.outputPath)
+            )
+
+
+
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("KML Importer Tool")
     root.geometry("500x500")
-    UserFrame(root)
+    UserMain(root)
     root.rowconfigure(0, weight=1)
     root.columnconfigure(0, weight=1)
     root.mainloop()
